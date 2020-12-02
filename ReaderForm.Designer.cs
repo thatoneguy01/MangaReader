5d905b7354a6d842a9be7de052a9fc0bc4c4954b
Daniel Scott <daniel.scott93@gmail.com>
2020-12-01 21:33:24 -0800

    Add project files.

---
+++ ReaderForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace MangaReader
+{
+    partial class ReaderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReaderForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1181, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(373, 224);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // ReaderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 841);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "ReaderForm";
+            this.Text = "MangaReader";
+            this.Load += new System.EventHandler(this.MangaReader_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
---
+++ Scraper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace MangaReader
+{
+    class Scraper
+    {
+        public static void ProcessPage(String url)
+        {
+            String mangaId = url.Substring(url.LastIndexOf('/')+1);
+            var html = GetHtmlFromPage(url);
+            var links = ExtractLinks(html);
+            links = FilterLinks(links, mangaId);
+            foreach (String link in links)
+            {
+                Console.WriteLine(link);
+            }
+        }
+
+        private static HtmlDocument GetHtmlFromPage(String url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(url);
+            return htmlDoc;
+        }
+
+        private static List<String> ExtractLinks(HtmlDocument htmlDoc)
+        {
+            List<String> links = new List<string>();
+            foreach (HtmlNode link in htmlDoc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                links.Add(link.GetAttributeValue("href", null));
+            }
+            return links;
+        }
+
+        private static List<String> FilterLinks(List<String> links, String mangaId)
+        {
+            List<String> filteredLinks = new List<string>();
+            Regex chapterRegex = new Regex(@"^https://manganelo.com/chapter/"+mangaId+"/.*$", RegexOptions.IgnoreCase);
+            foreach (String link in links)
+            {
+                if (chapterRegex.IsMatch(link))
+                {
+                    filteredLinks.Add(link);
+                }
+            }
+            return filteredLinks;
+        }
+    }
+}
